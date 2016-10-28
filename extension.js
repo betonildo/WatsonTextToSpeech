daff834dc77a8a85c953562bedac41c316728a5b
betonildo <gilbertoribeiropazdarosa@gmail.com>
2016-10-28 00:47:25 -0200

    Add: Text to speech using chunks of texts

--- extension.js
+++ extension.js
@@ -3,7 +3,9 @@
 let page = null;
 let audioTag = null; 
 let voiceTag = null;
-
+let watcherInterval = null;
+let audioBlobArray = [];
+let textsArray = [];
 const SELECTED_VOICE = "selectedVoice";
 const voices = ["en-US_LisaVoice", "pt-BR_IsabelaVoice", "en-US_MichaelVoice", "en-US_AllisonVoice"];
 const watsonApiUrl = "https://watson-api-explorer.mybluemix.net/text-to-speech/api/v1/synthesize?accept=audio%2Fogg%3Bcodecs%3Dopus";
@@ -13,24 +15,86 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 function onClickHandler(info, tab) {
     //page.alert(JSON.stringify(arguments));
     //selectAndSaveVoice(info.menuItemId);
-
+    clearPlayWatcherInterval();
     setupPageAndAudio();
     trySetVoiceOfVoicesAvailable(voices, info.menuItemId);
     speechText(info.selectionText);
 }
 
+function enqueueText(text) {
+    textsArray.push(text);
+}
+
+function dequeueText() {
+    let next = null;
+    if (textsArray.length > 0) {
+        next = textsArray.slice(0, 1)[0];
+        textsArray = textsArray.slice(1, textsArray.length);
+    }
+    
+    return next;
+}
+
+function enqueueBlob(audioBlob) {
+    audioBlobArray.push(audioBlob);
+}
+
+function dequeueBlob() {
+    let next = null;
+    if (audioBlobArray.length > 0) {
+        next = audioBlobArray.slice(0, 1)[0];
+        audioBlobArray = audioBlobArray.slice(1, audioBlobArray.length);
+    }
+    
+    return next;
+}
+
+
 function speechText(text) {
     
     if (text){
-        console.log(text);
+        let textsChunks = splitTextInto5KBArrayChunks(text);
+        textsChunks.forEach(enqueueText);
+        startAudioPlayWatcher(textsChunks.length);
+        console.log("Texts:",textsChunks);
         console.log("Bytes: " + text.length);
-        playTextAsVoice(text, audioTag, voiceTag);
+        let firstText = dequeueText();
+        playTextAsVoice(firstText, audioTag, voiceTag);
     }        
+}
+
+function splitTextInto5KBArrayChunks(text) {
+    let words = text.split(' ');
+    
+    let chunks = [];
+    let currentChunk = "";
+    let tempText = "";
+    let word = "";
+    for (let i = 0; i < words.length; i++) {
+        word = words[i];
+        tempText = currentChunk + word;
+        if (tempText.length > 100 && tempText.endsWith('.')) {
+            chunks.push(tempText);            
+            currentChunk = "";
+        }
+        else if (tempText.length > 500) {
+            chunks.push(tempText);            
+            currentChunk = "";
+        }
+        else {
+            currentChunk += word + ' ';
+        }
+    }
+
+    if (currentChunk !== "")
+        chunks.push(currentChunk);
+
+    return chunks;
 }
 
 function playTextAsVoice(textToSpeech, audioTag, voiceTag) {
     
-    let audioPostUrl = watsonApiUrl + "&voice=" + getVoice();
+     let audioPostUrl = watsonApiUrl + "&voice=" + getVoice();
 
     fetch(audioPostUrl, {
         method: 'post',
@@ -41,14 +105,61 @@ function playTextAsVoice(textToSpeech, audioTag, voiceTag) {
             'Content-Type' : 'application/json',
             'Accept' : 'audio/ogg'
         })
-	})
-    .then(response => response.blob())
-    .then(audioBytesArray => {
-        voiceTag.src = URL.createObjectURL(audioBytesArray);
-        audioTag.load();
-        audioTag.play();
     })
-    .catch(console.error);
+    .then(response => response.blob())
+    .then(audioBlob => {
+        enqueueBlob(audioBlob);
+        let nextText = dequeueText();
+        if (nextText) {            
+            playTextAsVoice(nextText, audioTag, voiceTag);
+        }
+    })
+    .catch( error => {
+        clearPlayWatcherInterval();
+        console.error("Some error occured!");
+        console.error(error);
+        audioBlobArray = [];
+        textsArray = [];
+    });
+}
+
+function startAudioPlayWatcher(withBlobsLength) {
+    let blobsCount = 0;
+    let isPlaying = false;
+    watcherInterval = setInterval(_ => {
+
+        if (!isPlaying) {
+            let nextBlob = dequeueBlob();
+            if (nextBlob) {
+                voiceTag.src = URL.createObjectURL(nextBlob);
+                audioTag.load();
+                audioTag.play();
+                blobsCount++;
+                isPlaying = true;
+                audioTag.addEventListener("ended", function(){
+                    console.log("ended");
+                    console.log(audioBlobArray);
+                    console.log(textsArray);
+                    isPlaying = false;
+                });
+                
+            }
+            else if (blobsCount === withBlobsLength){
+                clearPlayWatcherInterval();
+            }
+        }
+    }, 500);
+}
+
+function clearPlayWatcherInterval() {
+    if(watcherInterval) clearInterval(watcherInterval);
+    if(audioTag && voiceTag){
+        audioTag.pause();
+        audioTag.load();
+        voiceTag.src = "";        
+    } 
+    audioBlobArray = [];
+    textsArray = [];
 }
 
 function setupPageAndAudio() {
